39c04039311d2d20e4d560bf570e235a3a0a104c
Pxxl13 <luca.schneider@gmx.ch>
2016-10-18 15:00:20 +0200

    Added login and register fragment

--- app/src/main/java/testat/hsr/gadgeothek/GadgeothekActivity.java
+++ app/src/main/java/testat/hsr/gadgeothek/GadgeothekActivity.java
@@ -7,12 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class GadgeothekActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_gadgeothek);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 }
