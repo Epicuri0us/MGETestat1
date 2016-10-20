dfe98f211527ed8ec0839b822970a2312f8266a3
Pxxl13 <luca.schneider@gmx.ch>
2016-10-20 09:55:09 +0200

    added button presses + added connection to the server

--- app/src/main/java/testat/hsr/gadgeothek/LoginActivity.java
+++ app/src/main/java/testat/hsr/gadgeothek/LoginActivity.java
@@ -1,22 +1,34 @@
 package testat.hsr.gadgeothek;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import testat.hsr.gadgeothek.communication.LoginHelper;
 import testat.hsr.gadgeothek.communication.RegisterHelper;
 import testat.hsr.gadgeothek.layout.LoginFragment;
 import testat.hsr.gadgeothek.layout.RegisterFragment;
+import testat.hsr.gadgeothek.service.Callback;
+import testat.hsr.gadgeothek.service.LibraryService;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener, RegisterHelper {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,
+        RegisterHelper, LoginHelper {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        LibraryService.setServerAddress("http://mge1.dev.ifs.hsr.ch/public");
         handleFragment(new LoginFragment(), false);
     }
 
@@ -25,10 +37,51 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             case R.id.register:
                 handleFragment(new RegisterFragment(),true);
                 break;
-            case R.id.email_sign_in_button:
+            default:
                 break;
         }
 
+    }
+
+    @Override
+    public void RegisterHelper(final String email, final String password, String name, String matrikelnummer) {
+        LibraryService.register(email, password, name, matrikelnummer, new Callback<Boolean>() {
+            @Override
+            public void onCompletion(Boolean input) {
+                Toast.makeText(getApplicationContext(), "Registration ok", Toast.LENGTH_SHORT).show();
+                LoginHelper(email,password);
+                // TODO: 20.10.2016 test argumentübergabe mit backbutton (autofill in fragment)
+                //Intent intent = new Intent(getBaseContext(), GadgeothekActivity.class);
+                //startActivity(intent);
+                // FragmentManager fm = getSupportFragmentManager();
+                //fm.popBackStack();
+            }
+
+            @Override
+            public void onError(String message) {
+                Log.d(TAG, "onError() called with: message = [" + message + "]");
+            }
+        });
+    }
+
+    @Override
+    public void LoginHelper(String email, String password) {
+        // TODO: 20.10.2016 example login, to be removed
+        String userExample = "m@hsr.ch";
+        String passwordExample = "12345";
+        LibraryService.login(userExample, passwordExample, new Callback<Boolean>() {
+            @Override
+            public void onCompletion(Boolean input) {
+                Toast.makeText(getApplicationContext(), "Login ok", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getBaseContext(), GadgeothekActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onError(String message) {
+                Log.d(TAG, "onError() called with: message = [" + message + "]");
+            }
+        });
     }
 
     private void handleFragment(Fragment fragment, boolean replace){
@@ -40,11 +93,5 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             ft.add(R.id.fragment,fragment);
         }
         ft.commit();
-    }
-
-    // TODO: 19.10.2016
-    @Override
-    public void RegisterHelper(String email, String password, String name, String matrikelnummer) {
-        //has to be implemented
     }
 }
--- app/src/main/java/testat/hsr/gadgeothek/layout/LoginFragment.java
+++ app/src/main/java/testat/hsr/gadgeothek/layout/LoginFragment.java
@@ -4,21 +4,39 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import testat.hsr.gadgeothek.R;
+import testat.hsr.gadgeothek.communication.LoginHelper;
+
+import static android.content.ContentValues.TAG;
 
 public class LoginFragment extends Fragment {
+
+    LoginHelper loginHelper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_login, container, false);
         root.findViewById(R.id.register)
                 .setOnClickListener((View.OnClickListener)getActivity());
+        Button b = (Button)root.findViewById(R.id.email_sign_in_button);
+        final EditText editMail = (EditText)root.findViewById(R.id.emailLogin);
+        final EditText editPassword = (EditText)root.findViewById(R.id.passwordLogin);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginHelper.LoginHelper(editMail.getText().toString(),editPassword.getText().toString());
+            }
+        });
         return root;
     }
 
@@ -30,5 +48,17 @@ public class LoginFragment extends Fragment {
             throw new
                     AssertionError("Activity must implement View.OnClickListener!");
         }
+        if (!(context instanceof LoginHelper)){
+            throw new
+                    AssertionError("Activity must implement LoginHelper");
+        }
+        try {
+            loginHelper = (LoginHelper) context;
+        }
+        catch(ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement LoginHelper");
+        }
     }
+
+
 }
