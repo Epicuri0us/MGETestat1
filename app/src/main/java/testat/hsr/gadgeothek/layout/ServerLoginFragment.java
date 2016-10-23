63a75b4a4f4960260c569bab62fd05ef6f7237ed
Leo Zurbriggen <leo@leoz.ch>
2016-10-23 13:53:05 +0200

    Server login fragment button sets the LibraryService server address

--- app/src/main/java/testat/hsr/gadgeothek/layout/ServerLoginFragment.java
+++ app/src/main/java/testat/hsr/gadgeothek/layout/ServerLoginFragment.java
@@ -9,24 +9,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import testat.hsr.gadgeothek.R;
+import testat.hsr.gadgeothek.service.LibraryService;
 
 public class ServerLoginFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
         View root = inflater.inflate(R.layout.fragment_server_login, container, false);
 
-        final EditText serverAddress = (EditText)root.findViewById(R.id.serverAddress);
+        final EditText editServerAddress = (EditText)root.findViewById(R.id.serverAddress);
 
         root.findViewById(R.id.saveServerSettings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String serverAddress = editServerAddress.getText().toString();
+                LibraryService.setServerAddress(serverAddress);
 
+                Toast.makeText(getActivity().getApplicationContext(), "Set server address to: " + serverAddress, Toast.LENGTH_SHORT).show();
             }
         });
 
